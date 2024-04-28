b4a378967ddc20328d9e242b2d755313b909a250
Espen Sørby <espen.sorby@rikstv.no>
2024-04-28 16:36:00 +0200

    Add ShoppingBag button from shadcn to Navbar

--- app/(site)/components/Navbar.tsx
+++ app/(site)/components/Navbar.tsx
@@ -1,7 +1,9 @@
 'use client';
 
+import { Button } from '@/components/ui/button';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
+import { ShoppingBag } from 'lucide-react';
 
 const links = [
   { name: 'Home', href: '/' },
@@ -18,7 +20,7 @@ export default function Navbar() {
         className="flex items-center justify-between mx-auto max-w-2xl px-4 sm:px-6
     lg:max-w-7xl">
         <Link href="/">
-          <h1 className="text-4xl font-bold">
+          <h1 className="text-2xl md:text-4xl font-bold">
             Next <span className="text-primary">Commerce</span>
           </h1>
         </Link>
@@ -41,6 +43,16 @@ export default function Navbar() {
             </div>
           ))}
         </nav>
+        <div className="flex divide-x border-r sm:border-l">
+          <Button
+            variant={'outline'}
+            className="flex flex-col gap-y-1.5 w-12 h-12 sm:w-20 sm:h-20 md:w-24 m:h-24 rounded-none">
+            <ShoppingBag />
+            <span className="hidden text-xs font-semibold text-gray-500 sm:block">
+              Cart
+            </span>
+          </Button>
+        </div>
       </div>
     </header>
   );
